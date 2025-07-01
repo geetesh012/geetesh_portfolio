ab8e01fc1defad1d4652abaa2b4af43708847e49
geetesh012 <geeteshkankonkar@gmail.com>
2025-07-01 11:52:49 +0530

    fork-btn link added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/geetesh012"
+                href="https://github.com/geetesh012/geetesh_portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
